fca1248c499fd63fd5f62228ae90f0b713e4b1ba
clementstutz <cl.stutz@laposte.net>
2022-12-15 20:41:57 +0100

    plus belle table

--- UnitTesting/Chapter12Test.cs
+++ UnitTesting/Chapter12Test.cs
@@ -225,5 +225,111 @@ namespace RT.UnitTesting
             Scene.current.Clear();
             Console.WriteLine("Image created.");
         }
+
+        [Test, Order(5)]
+        public void T05_PuttingItAllTogether_better()
+        {
+            if (Scene.current == null)
+            {
+                new Scene();
+            }
+
+            Scene.current.Clear();
+
+            Light light = new Light(new Point(-4, 4, -3), Color.white);
+
+            //Create a cube room
+            //Floor
+            Cube floor = new Cube();
+            floor.material.pattern = new Patterns.CheckersPattern();
+            floor.material.Shininess = 200.0;
+            floor.material.Reflectivity = 0.2;
+            floor.material.pattern.matrix = Mat4.ScaleMatrix(0.2, 0.2, 0.2);
+            floor.SetMatrix(Mat4.TranslateMatrix(0, 0.1, 0) * Mat4.ScaleMatrix(10, 0.1, 10));
+
+            //Ceiling
+            Cube ceiling = new Cube();
+            ceiling.material.color = new Color(0.9, 0.9, 0.95);
+            ceiling.SetMatrix(Mat4.TranslateMatrix(0, 5, 0) * Mat4.ScaleMatrix(10, 0.1, 10));
+
+            //Back  Wall
+            Cube backWall = new Cube();
+            backWall.material.pattern = new Patterns.SolidColorPattern(new Color(0.6, 0.1, 0.05));
+            backWall.material.color = new Color(0.6, 0.1, 0.05);
+            backWall.material.Reflectivity = 0;
+            backWall.SetMatrix(Mat4.TranslateMatrix(0, 0, 0) * Mat4.ScaleMatrix(5, 5, 6));
+
+            //Mirror
+            Cube mirror = new Cube();
+            mirror.material.color = Color.black;
+            mirror.material.Shininess = 300;
+            mirror.material.Reflectivity = 1;
+            mirror.SetMatrix(Mat4.TranslateMatrix(0, 2, 5.9) * Mat4.ScaleMatrix(2, 1, 0.005));
+
+            //Table
+            Cube tableTop = new Cube();
+            tableTop.SetMatrix(Mat4.TranslateMatrix(0, 1, 0) * Mat4.ScaleMatrix(2, 0.1, 1));
+            tableTop.material.color = new Color(0.6471, 0.3843, 0.2196);
+
+            Cube leg1 = new Cube();
+            leg1.SetMatrix(Mat4.TranslateMatrix(-1.7, 0, -0.9) * Mat4.ScaleMatrix(0.1, 1, 0.1));
+            leg1.material.color = new Color(0.6471, 0.3843, 0.2196);
+
+            Cube leg2 = new Cube();
+            leg2.SetMatrix(Mat4.TranslateMatrix(1.7, 0, -0.9) * Mat4.ScaleMatrix(0.1, 1, 0.1));
+            leg2.material.color = new Color(0.6471, 0.3843, 0.2196);
+
+            Cube leg3 = new Cube();
+            leg3.SetMatrix(Mat4.TranslateMatrix(1.7, 0, 0.9) * Mat4.ScaleMatrix(0.1, 1, 0.1));
+            leg3.material.color = new Color(0.6471, 0.3843, 0.2196);
+
+            Cube leg4 = new Cube();
+            leg4.SetMatrix(Mat4.TranslateMatrix(-1.7, 0, 0.9) * Mat4.ScaleMatrix(0.1, 1, 0.1));
+            leg4.material.color = new Color(0.6471, 0.3843, 0.2196);
+
+            //Sphere
+            Sphere sphere1 = new Sphere();
+            sphere1.SetMatrix(Mat4.TranslateMatrix(0.9, 1.5, 0) * Mat4.ScaleMatrix(0.4, 0.4, 0.4));
+            sphere1.material.RefractIndex = RefractiveIndex.Glass;
+            sphere1.material.Diffuse = 0.0;
+            sphere1.material.Transparency = 1;
+            sphere1.canCastShadows = false;
+
+            Sphere sphere2 = new Sphere();
+            sphere2.SetMatrix(Mat4.TranslateMatrix(-0.9, 1.5, 0) * Mat4.ScaleMatrix(0.4, 0.4, 0.4));
+            sphere2.material.color = new Color(0, 0.1, 0);
+            sphere2.material.Diffuse = 0.2;
+            sphere2.material.Shininess = 100;
+            sphere2.material.Reflectivity = 1.0;
+            sphere2.material.Transparency = 0.2;
+            sphere2.material.RefractIndex = RefractiveIndex.Vacuum;
+            sphere2.canCastShadows = true;
+
+            Cylinder vase = new Cylinder();
+            // How do I just cap the top of the cylinder?
+            vase.minimum = -1;
+            vase.maximum = 1;
+            vase.isClosed = true;
+            vase.SetMatrix(Mat4.TranslateMatrix(0, 1.5, 0) * Mat4.ScaleMatrix(0.3, 0.4, 0.3));
+            vase.material.color = new Color(0.6588, 0.7882, 0.8824);
+            vase.material.RefractIndex = RefractiveIndex.Water;
+            vase.material.Diffuse = 0.0;
+            vase.material.Transparency = 0.95;
+            vase.canCastShadows = false;
+
+            Cube tige = new Cube();
+            tige.SetMatrix(Mat4.TranslateMatrix(0, 1.5, 0) * Mat4.RotateZMatrix(Math.PI/8) * Mat4.ScaleMatrix(0.03, 0.8, 0.03));
+            tige.material.color = new Color(0, 0.6, 0);
+
+            Camera camera = new Camera(200, 132, Math.PI / 3);
+            camera.ViewTransform(new Point(0, 2.2, -5),
+                                 new Point(0, 1, 4),
+                                 new Vector(0, 1, 0));
+
+            Canvas canvas = Scene.current.Render(camera, 3);
+            Save.SaveCanvas(canvas, "Chapter_12_CubeRoom_better");
+            Scene.current.Clear();
+            Console.WriteLine("Image created.");
+        }
     }
 }
